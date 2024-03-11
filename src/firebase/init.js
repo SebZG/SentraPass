f1b6e227d1952c3b02740961944932fa24be412e
SebZG <sebastian.zapata.g@gmail.com>
2024-03-11 17:03:08 +0000

    Adjust firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -7,7 +7,6 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-   databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
