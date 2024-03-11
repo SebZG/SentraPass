72a3f3e3b9083768e706d05b5d4252af29359c69
SebZG <sebastian.zapata.g@gmail.com>
2024-03-11 16:15:04 +0000

    Add starter files

---
+++ src/firebase/init.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+// Firebase configuration
+const firebaseConfig = {
+   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+   measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore();
+const analytics = getAnalytics(app);
