926f58822f22b71886d7017fc013a80e3f63a67f
SebZG <sebastian.zapata.g@gmail.com>
2024-03-12 01:47:41 +0000

    Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <div>
-      <h1>SentraPass</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1>SentraPass</h1>
+		</div>
+	)
 }
 
 export default App;
