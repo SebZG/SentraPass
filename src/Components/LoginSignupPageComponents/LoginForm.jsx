f5497f5bdbae7a7105b65257e562419a8e877e82
anadomuta <ana.domuta@hotmail.de>
2024-03-13 21:13:00 +0000

    adds styling and logo to signup form

--- src/Components/LoginSignupPageComponents/LoginForm.jsx
+++ src/Components/LoginSignupPageComponents/LoginForm.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Logo from "../../assets/images/finalLogo.jpg";
+import "./LoginForm.css";
 
 const LoginForm = () => {
   return (
-    <div className="color-overlay d-flex justify-content-center align-items-center">
-      <Form className="rounded p-4 p-sm-3">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter Email" />
+    <Container
+      id="main-container"
+      className="d-flex align-items-center justify-content-center h-100"
+    >
+      <Form id="sign-in-form" className="text-center p-3">
+        <img className="mb-4 sentra-logo" src={Logo} alt="Own logo" />
+        <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
+        <Form.Group controlId="sign-in-email-address">
+          <Form.Control
+            type="email"
+            size="lg"
+            placeholder="Email address"
+            autoComplete="username"
+            className="position-relative"
+          />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter Password" />
+        <Form.Group className="mb-3" controlId="sign-in-password">
+          <Form.Control
+            type="password"
+            size="lg"
+            placeholder="Password"
+            autoComplete="current-password"
+            className="position-relative"
+          />
         </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
+        <Form.Group
+          className="d-flex justify-content-center mb-4"
+          controlId="remember-me"
+        >
+          <Form.Check label="Remember me" />
+        </Form.Group>
+        <div className="d-grid">
+          <Button id="sign-in-button" size="lg">
+            Sign in
+          </Button>
+        </div>
       </Form>
-    </div>
+    </Container>
   );
 };
 
