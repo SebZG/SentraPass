d03cff6f2c3ff7cd2df5410f5b948a1dfb91f9e6
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-03-13 20:19:48 +0000

    fix: changed logo size in header and footer

--- src/Components/GeneralComponents/Footer/Footer.jsx
+++ src/Components/GeneralComponents/Footer/Footer.jsx
@@ -1,28 +1,64 @@
-import './Footer.css'
+import "./Footer.css";
+import logo from "../../../assets/images/finalLogo.jpg";
 
 const Footer = () => {
-	return (
-		<footer id='footer' className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top fixed-bottom">
-			<p className="col-md-4 mb-0 text-body-secondary">© {new Date().getFullYear()} SentraPass</p>
+  return (
+    <footer
+      id="footer"
+      className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top fixed-bottom"
+    >
+      <p className="col-md-4 mb-0 text-body-secondary">
+        © {new Date().getFullYear()} SentraPass
+      </p>
 
-			<a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-				{/* LOGO GOES HERE*/}
-				<span className="fs-4">SentraPass</span>
-			</a>
+      <a
+        href="/"
+        className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+      >
+        <img
+          src={logo}
+          className="logo"
+          alt="logo"
+          width="175"
+          height="175"
+          loading="lazy"
+        />
+        <span className="fs-4">SentraPass</span>
+      </a>
 
-			<ul className="nav col-md-4 justify-content-end">
-				<li className="btn"><a href="#" className="">Home</a></li>
-				<li className="btn"><a href="#" className="">Login</a></li>
-				<li className="btn"><a href="#" className="">Dashboard</a></li>
-				<li className="btn"><a href="#" className="">My Account</a></li>
-				<li className="btn"><a href="#" className="">Contact/About</a></li>
-			</ul>
-		</footer>
-	)
-}
+      <ul className="nav col-md-4 justify-content-end">
+        <li className="btn">
+          <a href="#" className="">
+            Home
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Login
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Dashboard
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            My Account
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Contact/About
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 export default Footer;
 
-// see line 15 for necessary logo change
+// see line 15 for necessary logo change ✅
 // see lines 19 - 23 for links to be added at later date
 // edit CSS to alter placement of elements inside footer
 // edit CSS to alter color
--- src/Components/GeneralComponents/Header/Header.jsx
+++ src/Components/GeneralComponents/Header/Header.jsx
@@ -2,14 +2,20 @@ import "./Header.css";
 import logo from "../../../assets/images/finalLogo.jpg";
 
 const Header = () => {
-
   return (
     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a
         href="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
       >
-        <img src={logo} className='logo' alt='logo' width='75' height='75' loading='lazy' />
+        <img
+          src={logo}
+          className="logo"
+          alt="logo"
+          width="75"
+          height="75"
+          loading="lazy"
+        />
 
         <span className="fs-4">SentraPass</span>
       </a>
@@ -46,7 +52,7 @@ const Header = () => {
 };
 export default Header;
 
-// add logo on line 7
+// add logo on line 7 ✅
 // see lines 11 - 16 for links to be added at later date
 // see line 19 for button add link to login/register page
 // edit CSS to alter placement of elements inside header
