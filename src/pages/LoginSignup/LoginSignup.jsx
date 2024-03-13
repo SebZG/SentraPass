04d09bbe8d3a6b0f74a325b01a6b6483829e65a2
anadomuta <ana.domuta@hotmail.de>
2024-03-13 19:49:51 +0000

    adds login form to login sign up page

--- src/pages/LoginSignup/LoginSignup.jsx
+++ src/pages/LoginSignup/LoginSignup.jsx
@@ -1,8 +1,16 @@
-import './LoginSignup.css'
+import React from "react";
+import "./LoginSignup.css";
+import LoginForm from "../../Components/LoginSignupPageComponents";
+import Header from "../../Components/GeneralComponents/Header";
+import Footer from "../../Components/GeneralComponents/Footer";
 
 const LoginSignup = () => {
-   return (
-      <div>LoginSignup</div>
-   )
-}
+  return (
+    <div>
+      <Header />
+      <LoginForm />
+    </div>
+  );
+};
+
 export default LoginSignup;
