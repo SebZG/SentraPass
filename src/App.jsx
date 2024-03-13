8d6d49205b5d62217445f9fbca209347d4450a88
SebZG <sebastian.zapata.g@gmail.com>
2024-03-13 06:07:04 +0000

    Add Scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing'
+import AboutContact from './pages/AboutContact'
 import LoginSignup from './pages/LoginSignup'
 import Dash from './pages/Dash'
 import Account from './pages/Account'
@@ -12,10 +13,11 @@ function App() {
 			<Routes>
 
 				<Route path='/' element={<Landing />} />
+				<Route path='/aboutcontact' element={<AboutContact />} />
 				<Route path='/login' element={<LoginSignup />} />
 				<Route path='/dash' element={<Dash />} />
 				<Route path='/account' element={<Account />} />
-				
+
 			</Routes>
 		</Router>
 	)
