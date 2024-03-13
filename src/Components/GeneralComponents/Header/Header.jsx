3d7446330490e8ac744ad74115df8e3e362f52bb
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-03-13 19:32:03 +0000

    fix: added favicon and logo in navbar

--- src/Components/GeneralComponents/Header/Header.jsx
+++ src/Components/GeneralComponents/Header/Header.jsx
@@ -1,23 +1,48 @@
-import './Header.css'
+import "./Header.css";
+import logo from "../../../assets/images/finalLogo.jpg";
 
 const Header = () => {
-	return (
-		<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-			<a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-				{/* LOGO GOES HERE*/}
-				<span className="fs-4">SentraPass</span>
-			</a>
+  return (
+    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a
+        href="/"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+      >
+        <img src={logo} className='logo' alt='logo' width='75' height='75' loading='lazy' />
 
-			<ul className="nav nav-pills">
-				<li className="btn"><a href="#" className="">Home</a></li>
-				<li className="btn"><a href="#" className="">Login/Register</a></li>
-				<li className="btn"><a href="#" className="">Dashboard</a></li>
-				<li className="btn"><a href="#" className="">My Account</a></li>
-				<li className="btn"><a href="#" className="">Contact/About</a></li>
-			</ul>
-		</header>
-	)
-}
+        <span className="fs-4">SentraPass</span>
+      </a>
+
+      <ul className="nav nav-pills">
+        <li className="btn">
+          <a href="#" className="">
+            Home
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Login/Register
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Dashboard
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            My Account
+          </a>
+        </li>
+        <li className="btn">
+          <a href="#" className="">
+            Contact/About
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
 export default Header;
 
 // add logo on line 7
