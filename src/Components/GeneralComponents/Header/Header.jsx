91e09084bdabe9f68fcc5c065241887acf924c19
SebZG <sebastian.zapata.g@gmail.com>
2024-03-13 19:15:21 +0000

    fix footer

--- src/Components/GeneralComponents/Header/Header.jsx
+++ src/Components/GeneralComponents/Header/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css'
 
 const Header = () => {
 	return (
-		<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+		<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom sticky-top">
 			<a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
 				{/* LOGO GOES HERE*/}
 				<span className="fs-4">SentraPass</span>
