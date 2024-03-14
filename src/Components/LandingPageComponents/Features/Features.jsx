141240a4cab1d3234f40293c87571677c501932a
C Grogan <conorgrogan23@gmail.com>
2024-03-14 23:17:38 +0000

    added a accordion

--- src/Components/LandingPageComponents/Features/Features.jsx
+++ src/Components/LandingPageComponents/Features/Features.jsx
@@ -9,8 +9,9 @@ const Features = () => {
 					<div className="col-md-4">
 						<div className="feature-item">
 							<h3>Secure storage for all your password needs.</h3>
-							<p>Browse the web with peace of mind knowing that all your passwords are securely stored in SentraPass.
-								With the click of a button you can reveal all your passwords and then quickly hide them from prying eyes.</p>
+							<p>Between online shopping, entertainment and work we have passwords for all kinds of things in life
+                     and it can get difficult to remember all of them. With SentraPass you can store your passwords
+                     and set categories for all your accounts avoiding confusion and hassle of remembering them all. </p>
 						</div>
 					</div>
 					<div className="col-md-4">
@@ -23,8 +24,8 @@ const Features = () => {
 						<div className="feature-item">
 							<h3>Passwords for all your accounts</h3>
 							<p>Between online shopping, entertainment and work we have passwords for all kinds of things in life
-								and it can get difficult to remember all of them. With SentraPass you can store your passwords
-								and set categories for all your accounts avoiding confusion and hassle of remembering them all. </p>
+                		and it can get difficult to remember all of them. With SentraPass you can store your passwords
+                		and set categories for all your accounts avoiding confusion and hassle of remembering them all. </p>
 						</div>
 					</div>
 				</div>
