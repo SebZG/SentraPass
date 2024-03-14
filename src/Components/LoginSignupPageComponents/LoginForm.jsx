b2c9e16f2e51b3698ef612c474d7d7f28732e69f
anadomuta <ana.domuta@hotmail.de>
2024-03-14 17:23:57 +0000

    adds logic to switch between login and account creation states

--- src/Components/LoginSignupPageComponents/LoginForm.jsx
+++ src/Components/LoginSignupPageComponents/LoginForm.jsx
@@ -13,6 +13,19 @@ const LoginForm = () => {
     confirmPassword: "",
   });
 
+  const [loginMode, setLoginMode] = useState(false);
+
+  const handleModeChange = () => {
+    setLoginMode(!loginMode);
+    // Clear the confirm password field when switching modes
+    if (loginMode) {
+      setFormData({
+        ...formData,
+        confirmPassword: "",
+      });
+    }
+  };
+
   const [rememberMe, setRememberMe] = useState(false);
 
   useEffect(() => {
@@ -59,7 +72,7 @@ const LoginForm = () => {
       <Form id="sign-in-form" className="text-center p-3">
         <img className="mb-4 sentra-logo" src={Logo} alt="Own logo" />
         <h1 id="title" className="mb-3 fs-3 fw-normal">
-          Create an account
+          {loginMode ? "Login" : "Create an Account"}
         </h1>
         <Form.Group className="mb-3" controlId="sign-in-email-address">
           <Form.Control
@@ -85,7 +98,10 @@ const LoginForm = () => {
             }
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="confirm-password">
+        <Form.Group
+          className={`mb-3 ${loginMode ? "d-none" : ""}`}
+          controlId="confirm-password"
+        >
           <Form.Control
             type="password"
             size="lg"
@@ -110,13 +126,15 @@ const LoginForm = () => {
         </Form.Group>
         <div className="d-grid mb-3">
           <Button id="sign-in-button" size="lg" onClick={handleFormSubmit}>
-            Sign up
+            {loginMode ? "Login" : "Sign up"}
           </Button>
         </div>
-        <p>Already registered?</p>
+        <p> {loginMode ? "Don't have an account?" : "Already registered?"}</p>
         <span className="line">
           {/* {} */}
-          <a href="#">Login</a>
+          <a href="#" onClick={handleModeChange}>
+            {loginMode ? "Create an account" : "Login"}
+          </a>
         </span>
       </Form>
     </Container>
