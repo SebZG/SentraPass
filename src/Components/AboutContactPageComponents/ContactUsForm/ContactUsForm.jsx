05ec6fca55fd401df9977031525fea771b5aa2e8
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-16 12:06:25 +0000

    added npm email js to create functioning email form. Css stylings applied to code. Refactored react component flow, created buttons on initial landing of contact/about to improve user experience

--- src/Components/AboutContactPageComponents/ContactUsForm/ContactUsForm.jsx
+++ src/Components/AboutContactPageComponents/ContactUsForm/ContactUsForm.jsx
@@ -1,9 +1,48 @@
-import './ContactUsForm.css'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactUsForm.css";
 
-function ContactUsForm() {
+// npm i @emailjs/browser
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "replace with service id",
+        "replace with template id",
+        form.current,
+        "replace with user id"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>ContactUsForm</div>
-  )
-}
+    <>
+      <div className="contact-form col-md-4">
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default ContactUsForm;
+export default Contact;
