5d8dfc5f661bff102659068886a6ba1d7dfe94e9
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-16 14:28:48 +0000

    added css styling to typewriter

--- src/Components/AboutContactPageComponents/TypewriterHeader/TypewriterHeader.jsx
+++ src/Components/AboutContactPageComponents/TypewriterHeader/TypewriterHeader.jsx
@@ -1,48 +1,51 @@
 import React, { useState, useEffect } from "react";
 import "./TypewriterHeader.css";
+
 const TypewriterEffect = () => {
   const [text, setText] = useState("");
-  const phrases = ["The Creators", "EdX Bootcamp"];
+  const phrases = ["Sentras Pass Creators", "EdX Bootcamp"];
+  const typingSpeed = 100;
+  const phraseChangeDelay = 1500;
 
   useEffect(() => {
     let currentPhraseIndex = 0;
     let currentCharIndex = 0;
     let typingTimeout;
-    let deletingTimeout;
 
     const typeText = () => {
       setText((prevText) =>
         phrases[currentPhraseIndex].slice(0, currentCharIndex + 1)
       );
-      currentCharIndex++;
-      if (currentCharIndex < phrases[currentPhraseIndex].length) {
-        typingTimeout = setTimeout(typeText, 100);
+
+      if (currentCharIndex < phrases[currentPhraseIndex].length - 1) {
+        currentCharIndex++;
+        typingTimeout = setTimeout(typeText, typingSpeed);
       } else {
-        deletingTimeout = setTimeout(deleteText, 1500);
+        typingTimeout = setTimeout(changePhrase, phraseChangeDelay);
       }
     };
 
-    const deleteText = () => {
-      setText((prevText) => {
-        if (prevText.length === 0) {
-          currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
-          currentCharIndex = 0;
-          return "";
-        }
-        return prevText.slice(0, prevText.length - 1);
-      });
-      deletingTimeout = setTimeout(deleteText, 50);
+    const changePhrase = () => {
+      currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+      currentCharIndex = 0;
+      typingTimeout = setTimeout(typeText, typingSpeed);
     };
 
-    typingTimeout = setTimeout(typeText, 1000);
+    typingTimeout = setTimeout(typeText, typingSpeed);
 
     return () => {
       clearTimeout(typingTimeout);
-      clearTimeout(deletingTimeout);
     };
-  }, []);
+  }, []); // empty dependency array for componentDidMount behavior
 
-  return <h1 className="creators col">{text}</h1>;
+  return (
+    <>
+      <div className="typewriter">
+        <h1 className="creators">{text}</h1>
+        <h1 className="flashing-text">_</h1>
+      </div>
+    </>
+  );
 };
 
 export default TypewriterEffect;
