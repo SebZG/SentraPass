434a66ac868f5ac3d3eb764c7310b612e1813f2b
anadomuta <ana.domuta@hotmail.de>
2024-03-16 14:50:22 +0000

    fix about me page

--- src/pages/AboutContact/AboutContact.jsx
+++ src/pages/AboutContact/AboutContact.jsx
@@ -1,192 +1,42 @@
-import React, { useState, useEffect } from "react";
-import "./AboutContact.css";
+import { useState } from "react";
+
 import Header from "../../Components/GeneralComponents/Header";
 import Footer from "../../Components/GeneralComponents/Footer";
 
-const creatorsdata = [
-  {
-    id: 1,
-    showSkills: false,
-    name: "Seb",
-    img: "src/assets/images/seb_profile_pic.png",
-    desc: "Back End Bandit & Server Sorcerer",
-    github: "https://github.com/SebZG",
-    linkedin: "https://www.linkedin.com/in/sebastian-z-g/",
-    email: "pCZ8x@example.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "SASS",
-      "JavaScript",
-      "React",
-      "Vue",
-      "Nuxt",
-      "NodeJS",
-      "Babel",
-      "Jquery",
-      "ES6",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-  {
-    id: 2,
-    showSkills: false,
-    name: "Ana",
-    img: "src/assets/images/ana_profile_pic.png",
-    desc: "React Sorceress & JavaScript Jester",
-    github: "https://github.com/anadomuta",
-    linkedin: "https://www.linkedin.com/in/ana-domuta/",
-    email: "pCZ8x@example.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "SASS",
-      "JavaScript",
-      "React",
-      "Vue",
-      "Nuxt",
-      "NodeJS",
-      "Babel",
-      "Jquery",
-      "ES6",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-  {
-    id: 3,
-    showSkills: false,
-    name: "Harry",
-    img: "src/assets/images/harry_profile_pic.png",
-    desc: "Script Juggler & Exception Exterminator",
-    github: "https://github.com/hpro97",
-    linkedin: "https://www.linkedin.com/in/harryprobert/",
-    email: "pCZ8x@example.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "SASS",
-      "JavaScript",
-      "React",
-      "Vue",
-      "Nuxt",
-      "NodeJS",
-      "Babel",
-      "Jquery",
-      "ES6",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-  {
-    id: 4,
-    showSkills: false,
-    name: "Aaron",
-    img: "src/assets/images/aaron_profile_pic.png",
-    desc: "Array Avenger & Coding Chameleon",
-    github: "https://github.com/aaron1490",
-    linkedin:
-      "https://www.linkedin.com/in/aaron-armstrong-%E8%89%BE%E4%BF%8A%E6%A8%82-80986ba5/",
-    email: "pCZ8x@example.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "SASS",
-      "JavaScript",
-      "React",
-      "Vue",
-      "Nuxt",
-      "NodeJS",
-      "Babel",
-      "Jquery",
-      "ES6",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-  {
-    id: 5,
-    showSkills: false,
-    name: "Conor",
-    img: "src/assets/images/conner_profile_pic.png",
-    desc: "Bootstrap Buccaneer & Script Scribe",
-    github: "https://github.com/Crar96",
-    linkedin: "https://www.linkedin.com/in/conor-grogan-a09a7a216/",
-    email: "pCZ8x@example.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "Bootstrap",
-      "React",
-      "Jquery",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-  {
-    id: 6,
-    showSkills: false,
-    name: "Tom",
-    img: "src/assets/images/tom_profile_pic.png",
-    desc: "CSS Magician & SASS Sauron",
-    github: "https://github.com/DrWho1369",
-    linkedin: "https://www.linkedin.com/in/thomas-baker-anutr-0b9758222/",
-    email: "tbaker.bhb@gmail.com",
-    skills: [
-      "Python",
-      "Django",
-      "HTML",
-      "CSS",
-      "SASS",
-      "JavaScript",
-      "React",
-      "NodeJS",
-      "Jquery",
-      "ES6",
-      "GIT",
-      "GITHUB",
-    ],
-  },
-];
+import ContactUsForm from "../../Components/AboutContactPageComponents/ContactUsForm";
+import "./AboutContact.css";
+import ContactCardFlipper from "../../Components/AboutContactPageComponents/ContactCardFlipper/ContactCardFlipper";
 
 const AboutContact = () => {
-  const [creators, setCreators] = useState(creatorsdata);
-  const setShowSkills = (id) => {
-    const currentCreator = creators.find((creator) => creator.id === id);
-    const creatorIndex = creators.indexOf(currentCreator);
-    const newCreators = [...creators];
-    newCreators[creatorIndex].showSkills = !currentCreator.showSkills;
-    setCreators(newCreators);
+  const [showCreatorCard, setShowCreatorCard] = useState(false);
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  const toggleCreatorCard = () => {
+    setShowCreatorCard(true);
+    setShowContactForm(false);
   };
+
+  const toggleContactForm = () => {
+    setShowContactForm(true);
+    setShowCreatorCard(false);
+  };
+
   return (
-    <>
+    <div id="about-contact">
       <Header />
       <div className="row">
-        <h1 className="creators">The Creators</h1>
+        <div className="button-container">
+          <button onClick={toggleCreatorCard}>Meet The Creators</button>
+          <button onClick={toggleContactForm}>Get In Contact</button>
+        </div>
+        <div className="col d-flex justify-content-center">
+          {showCreatorCard && <ContactCardFlipper />}
+          {showContactForm && <ContactUsForm />}
+        </div>
       </div>
-      <div className="row">
-        {creators.map((creator) => (
-          <div key={creator.id} className="col-md-6 col-lg-4">
-            <CreatorCard
-              setShowSkills={setShowSkills}
-              id={creator.id}
-              showSkills={creator.showSkills}
-              img={creator.img}
-              name={creator.name}
-              desc={creator.desc}
-              github={creator.github}
-              linkedin={creator.linkedin}
-              email={creator.email}
-              skills={creator.skills}
-            />
-          </div>
-        ))}
-      </div>
-
       <Footer />
-    </>
+    </div>
   );
 };
+
 export default AboutContact;
