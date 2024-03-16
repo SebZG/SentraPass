19cc2d7bd9d408af8ed555442b2f54641ee9c053
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-16 13:03:03 +0000

    made contact form work, hooked up with emailjs

--- src/Components/AboutContactPageComponents/ContactUsForm/ContactUsForm.jsx
+++ src/Components/AboutContactPageComponents/ContactUsForm/ContactUsForm.jsx
@@ -1,10 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./ContactUsForm.css";
 
-// npm i @emailjs/browser
-
 const Contact = () => {
+  const [emailSent, setEmailSent] = useState(false);
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -12,15 +11,16 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "replace with service id",
-        "replace with template id",
+        SENTRA_PASS_EMAILJS_SERVICE_ID,
+        SENTRA_PASS_EMAILJS_TEMPLATE_ID,
         form.current,
-        "replace with user id"
+        { publicKey: SENTRA_PASS_EMAILJS_PUBLIC_KEY }
       )
       .then(
         (result) => {
           console.log(result.text);
           console.log("message sent");
+          setEmailSent(true);
         },
         (error) => {
           console.log(error.text);
@@ -30,17 +30,24 @@ const Contact = () => {
 
   return (
     <>
-      <div className="contact-form col-md-4">
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form>
-      </div>
+      {!emailSent ? (
+        <div className="contact-form col-md-4">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      ) : (
+        <div className="email-sent-message">
+          <h1>Email Sent! Thank You!</h1>
+          <p>We'll get back to you as soon as we can.</p>
+        </div>
+      )}
     </>
   );
 };
