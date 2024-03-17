6dd856415004abe2d7b9eb3e2daddfad25c4b727
C Grogan <conorgrogan23@gmail.com>
2024-03-17 20:16:11 +0000

    push recent changes to accordion

--- src/Components/GeneralComponents/Header/Header.jsx
+++ src/Components/GeneralComponents/Header/Header.jsx
@@ -1,26 +1,40 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import './Header.css';
 
 const Header = () => {
+  // State for managing menu visibility
+  const [menuOpen, setMenuOpen] = useState(false);
 
-	return (
-		<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-			<a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-auto link-body-emphasis text-decoration-none">
-				<img src="src\assets\images\finalLogo.jpg" alt="Logo" height="40rem" className="" />
-				<span className="fs-4">SentraPass</span>
-			</a>
+  // Function to toggle menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-			<ul className="nav nav-pills">
-				<li className=""><Link to="/" className="">Home</Link></li>
-				<li className=""><Link to="/dash" className="">Dashboard</Link></li>
-				<li className=""><Link to="/account" className="">My Account</Link></li>
-				<li className=""><Link to="/aboutcontact" className="">Contact/About</Link></li>
-			</ul>
-		</header>
-	)
-}
+  return (
+    <header className="d-flex flex-wrap justify-content-between py-3 mb-4 border-bottom">
+      <div className="d-flex align-items-center mb-3 mb-md-0 me-auto link-body-emphasis text-decoration-none">
+        <img src="src\assets\images\finalLogo.jpg" alt="Logo" height="40rem" className="" />
+        <span className="fs-4">SentraPass</span>
+      </div>
+
+      {/* Menu button image */}
+      <img
+        src={menuOpen ? '../assets/images/closeIcon.png' : '../assets/images/menuIcon.png'}
+        alt="btn"
+        className="menu-btn"
+        onClick={toggleMenu}
+      />
+
+      {/* Menu items */}
+      <ul className={`nav nav-pills ${menuOpen ? 'show' : ''}`}>
+        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+        <li><Link to="/dash" onClick={toggleMenu}>Dashboard</Link></li>
+        <li><Link to="/account" onClick={toggleMenu}>My Account</Link></li>
+        <li><Link to="/aboutcontact" onClick={toggleMenu}>Contact/About</Link></li>
+      </ul>
+    </header>
+  );
+};
+
 export default Header;
-
-// edit CSS to alter placement of elements inside header
-// edit CSS to alter color
