e75137eaadb5f350eeb3f0b2fefed9041c4ca0a4
anadomuta <ana.domuta@hotmail.de>
2024-03-18 20:39:42 +0000

    adds draft details form on the account page

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,10 +1,12 @@
 import "./Account.css";
 import Navbar from "../../Components/AccountPageComponents/Navbar";
+import DetailsForm from "../../Components/AccountPageComponents/ChangeDetailsForm";
 
 const Account = () => {
   return (
     <div>
       <Navbar />
+      <DetailsForm />
     </div>
   );
 };
