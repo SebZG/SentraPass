a9d3b9eed32609558ada84968feb247a0a44e106
SebZG <sebastian.zapata.g@gmail.com>
2024-03-18 14:10:56 +0000

    Fix footer staying at bottom

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,22 +1,14 @@
-//global components//
-import Header from "../../Components/GeneralComponents/Header"
-import Footer from "../../Components/GeneralComponents/Footer"
-
 //specific components//
 import Hero from "../../Components/LandingPageComponents/Hero"
 import Features from "../../Components/LandingPageComponents/Features"
 import FAQ from "../../Components/LandingPageComponents/FAQ/FAQ";
 
-
-
 const Landing = () => {
    return (
       <div id="landing">
-         <Header />
          <Hero />
          <Features />
          <FAQ />
-         <Footer />
       </div>
    )
 }
