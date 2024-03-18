dcd49c4d9123699227a0b39920e72269fbcd9951
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-18 11:48:20 +0000

    scaffoled out snake game

--- src/Components/AboutContactPageComponents/SnakeGame/SnakeGame.jsx
+++ src/Components/AboutContactPageComponents/SnakeGame/SnakeGame.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS,
+} from "./constants";
+import { useInterval } from "./useInterval";
 
 function SnakeGame() {
+  const startGame = () => {};
+
+  const endGame = () => {};
+
+  const moveSnake = () => {};
+
+  const createApple = () => {};
+
+  const checkCollision = () => {};
+
+  const checkAppleCollision = () => {};
+
+  const gameLoop = () => {};
+
+  useEffect(() => {}, [snake, apple, gameOver]);
   return <div>SnakeGame</div>;
 }
 
