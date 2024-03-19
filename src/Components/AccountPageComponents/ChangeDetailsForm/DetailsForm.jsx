67e475dc2ab1a1875be3e8d7f7d0aa79e7f5bf79
anadomuta <ana.domuta@hotmail.de>
2024-03-19 19:42:08 +0000

    adds two card layout to account updates form

--- src/Components/AccountPageComponents/ChangeDetailsForm/DetailsForm.jsx
+++ src/Components/AccountPageComponents/ChangeDetailsForm/DetailsForm.jsx
@@ -1,87 +1,84 @@
-import React, { useState } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Container, Row, Col, Card, Button } from "react-bootstrap";
 
-const ChangeDetailsForm = () => {
-  const [formData, setFormData] = useState({
-    displayName: "",
-    email: "",
-    oldPassword: "",
-    newPassword: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Form submission logic here
-    console.log(formData);
-  };
-
+const AccountCard = () => {
   return (
     <Container>
       <Row className="justify-content-center">
-        <Col md={6}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="fullName">
-              <Form.Label>Display Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your full name"
-                name="fullName"
-                value={formData.fullName}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Row>
+                <Col md={6}>
+                  <ul>
+                    <li>
+                      <Button variant="link">Personal Details</Button>
+                    </li>
+                    <li>
+                      <Button variant="link">Change Password</Button>
+                    </li>
+                  </ul>
+                  <Button variant="danger">Logout</Button>
+                </Col>
 
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
+                <Col md={6}>
+                  {
+                    <Form>
+                      <Form.Group>
+                        <Form.Label>Display Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter your full name"
+                          name="displayName"
+                          required
+                        />
+                      </Form.Group>
 
-            <Form.Group controlId="oldPassword">
-              <Form.Label>Old Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter old password"
-                name="oldPassword"
-                value={formData.oldPassword}
-                onChange={handleChange}
-              />
-            </Form.Group>
+                      <Form.Group>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter email"
+                          name="email"
+                          required
+                        />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Save Changes
+                      </Button>
+                    </Form>
+                  }
+                  {
+                    <Form>
+                      <Form.Group controlId="oldPassword">
+                        <Form.Label>Old Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Enter old password"
+                          name="oldPassword"
+                        />
+                      </Form.Group>
 
-            <Form.Group controlId="newPassword">
-              <Form.Label> New Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter new password"
-                name="newm=Password"
-                value={formData.newPassword}
-                onChange={handleChange}
-              />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Save Changes
-            </Button>
-          </Form>
+                      <Form.Group controlId="newPassword">
+                        <Form.Label>New Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Enter new password"
+                          name="newPassword"
+                        />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Save Changes
+                      </Button>
+                    </Form>
+                  }
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
   );
 };
 
-export default ChangeDetailsForm;
+export default AccountCard;
--- src/Components/AccountPageComponents/Navbar/Navbar.jsx
+++ src/Components/AccountPageComponents/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div>
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <header
+        id="customHeader"
+        className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
+      >
         <a
           href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-auto link-body-emphasis text-decoration-none"
@@ -23,7 +27,7 @@ function Navbar() {
         <DropdownButton id="dropdown-basic-button" title="My Account">
           <Dropdown.Item href="/login">Login</Dropdown.Item>
           <Dropdown.Item href="/dash">Dashboard</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+          <Dropdown.Item href="#/logout">Logout</Dropdown.Item>
         </DropdownButton>
       </header>
     </div>
