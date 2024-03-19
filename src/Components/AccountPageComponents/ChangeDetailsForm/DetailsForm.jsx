fe1c33a6ed79764719add466cd0545ee07533d0e
anadomuta <ana.domuta@hotmail.de>
2024-03-19 20:26:01 +0000

    adds personal details and change password display upon click

--- src/Components/AccountPageComponents/ChangeDetailsForm/DetailsForm.jsx
+++ src/Components/AccountPageComponents/ChangeDetailsForm/DetailsForm.jsx
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
 import { Form, Container, Row, Col, Card, Button } from "react-bootstrap";
 
 const AccountCard = () => {
+  const [formData, setFormData] = useState({
+    displayName: "",
+    email: "",
+    oldPassword: "",
+    newPassword: "",
+  });
+
+  const [showPersonalDetails, setShowPersonalDetails] = useState(false);
+  const [showChangePassword, setShowChangePassword] = useState(false);
+  const [changesSaved, setChangesSaved] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setChangesSaved(true);
+  };
+
   return (
     <Container>
       <Row className="justify-content-center">
@@ -11,23 +36,41 @@ const AccountCard = () => {
                 <Col md={6}>
                   <ul>
                     <li>
-                      <Button variant="link">Personal Details</Button>
+                      <Button
+                        variant="link"
+                        onClick={() => {
+                          setShowPersonalDetails(true);
+                          setShowChangePassword(false);
+                          setChangesSaved(false);
+                        }}
+                      >
+                        Personal Details
+                      </Button>
                     </li>
                     <li>
-                      <Button variant="link">Change Password</Button>
+                      <Button
+                        variant="link"
+                        onClick={() => {
+                          setShowChangePassword(true);
+                          setShowPersonalDetails(false);
+                          setChangesSaved(false);
+                        }}
+                      >
+                        Change Password
+                      </Button>
                     </li>
                   </ul>
                   <Button variant="danger">Logout</Button>
                 </Col>
 
                 <Col md={6}>
-                  {
-                    <Form>
+                  {showPersonalDetails && (
+                    <Form onSubmit={handleSubmit}>
                       <Form.Group>
                         <Form.Label>Display Name</Form.Label>
                         <Form.Control
                           type="text"
-                          placeholder="Enter your full name"
+                          placeholder="Enter your display name"
                           name="displayName"
                           required
                         />
@@ -46,15 +89,17 @@ const AccountCard = () => {
                         Save Changes
                       </Button>
                     </Form>
-                  }
-                  {
-                    <Form>
+                  )}
+                  {showChangePassword && (
+                    <Form onSubmit={handleSubmit}>
                       <Form.Group controlId="oldPassword">
                         <Form.Label>Old Password</Form.Label>
                         <Form.Control
                           type="password"
                           placeholder="Enter old password"
                           name="oldPassword"
+                          value={formData.oldPassword}
+                          onChange={handleChange}
                         />
                       </Form.Group>
 
@@ -64,13 +109,15 @@ const AccountCard = () => {
                           type="password"
                           placeholder="Enter new password"
                           name="newPassword"
+                          value={formData.newPassword}
+                          onChange={handleChange}
                         />
                       </Form.Group>
                       <Button variant="primary" type="submit">
                         Save Changes
                       </Button>
                     </Form>
-                  }
+                  )}
                 </Col>
               </Row>
             </Card.Body>
