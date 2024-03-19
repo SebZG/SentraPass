e7002d3107d639016f8f4458b2b228a302cf5c9a
SebZG <sebastian.zapata.g@gmail.com>
2024-03-19 18:23:38 +0000

    Fix header/navbar imports

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,10 +2,12 @@
 import Hero from "../../Components/LandingPageComponents/Hero"
 import Features from "../../Components/LandingPageComponents/Features"
 import FAQ from "../../Components/LandingPageComponents/FAQ/FAQ";
+import Header from "../../Components/GeneralComponents/Header";
 
 const Landing = () => {
    return (
       <div id="landing">
+      <Header />
          <Hero />
          <Features />
          <FAQ />
--- src/pages/LoginSignup/LoginSignup.jsx
+++ src/pages/LoginSignup/LoginSignup.jsx
@@ -1,10 +1,13 @@
 import LoginForm from "../../Components/LoginSignupPageComponents";
 
+import Header from "../../Components/GeneralComponents/Header";
+
 import "./LoginSignup.css";
 
 const LoginSignup = () => {
   return (
     <div>
+      <Header />
       <LoginForm />
     </div>
   );
