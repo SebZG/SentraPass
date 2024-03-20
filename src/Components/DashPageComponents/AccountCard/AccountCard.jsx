b4b16f70746115f37474fdd9fa5d366711f834f7
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-20 20:13:22 +0000

    added styling to your accounts

--- src/Components/DashPageComponents/AccountCard/AccountCard.jsx
+++ src/Components/DashPageComponents/AccountCard/AccountCard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import "./AccountCard.css";
 function AccountCard(props) {
   return (
     <>
-      <div className="card col-4">
+      <div className="card">
         <ul className="list-group list-group-flush">
           <li className="list-group-item">{props.Account}</li>
           <li className="list-group-item">{props.UserName}</li>
           <li className="list-group-item">{props.Password}</li>
           <li className="list-group-item">
-            <a href={props.Link} target="_blank">
-              {props.Link}
+            <a href={props.Link} className="link" target="_blank">
+              Website Link
             </a>
           </li>
         </ul>
