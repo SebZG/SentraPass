5ca451ecf49015b1056408e1734833b6b5dddd63
Hpro97 <haxmo25@gmail.com>
2024-03-20 21:39:23 +0000

    header delete redundant comments

--- src/Components/GeneralComponents/Header/Header.jsx
+++ src/Components/GeneralComponents/Header/Header.jsx
@@ -27,7 +27,6 @@ const Header = () => {
         <span className="fs-4">SentraPass</span>
       </a>
 
-      {/* Display burger menu icon under media breakpoint */}
       <div
         className={`burger-menu ${isMenuOpen ? "open" : ""}`}
         onClick={toggleMenu}
@@ -39,7 +38,6 @@ const Header = () => {
         )}
       </div>
 
-      {/* Display nav items */}
       <ul className={`nav nav-pills ${isMenuOpen ? "open" : ""}`}>
         <li>
           <Link to="/" onClick={() => closeMenu()}>
@@ -63,7 +61,6 @@ const Header = () => {
         </li>
       </ul>
 
-      {/* Display dropdown menu under media breakpoint */}
       {isMenuOpen && (
         <div className="dropdown-menu">
           <ul className="nav flex-column">
@@ -95,68 +92,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// apply media query for burger menu on ul under 1200px
-
-//---------------------------------------------------//
-
-//working dropdown menu but not same styling
-
-// import { Link } from 'react-router-dom';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-// import { useState } from 'react';
-
-// import './Header.css';
-
-// const Header = () => {
-//   const [expanded, setExpanded] = useState(false);
-
-//   return (
-//     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-//       <Navbar.Brand href="#">
-//         <img src="src\assets\images\finalLogo.jpg" alt="Logo" height="40rem" className="" />
-//         <span className="fs-4">SentraPass</span>
-//       </Navbar.Brand>
-//       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//       <Navbar.Collapse id="responsive-navbar-nav">
-//         <Nav className="mr-auto">
-//           <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
-//           <Nav.Link as={Link} to="/dash" onClick={() => setExpanded(false)}>Dashboard</Nav.Link>
-//           <Nav.Link as={Link} to="/account" onClick={() => setExpanded(false)}>My Account</Nav.Link>
-//           <Nav.Link as={Link} to="/aboutcontact" onClick={() => setExpanded(false)}>Contact/About</Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-
-// export default Header;
-
-//------------------------------------------------//
-
-// import { Link } from 'react-router-dom';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-
-// const Header = () => {
-//   return (
-//     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-//       <Navbar.Brand href="/">
-//         <img src="src\assets\images\finalLogo.jpg" alt="Logo" height="40rem" className="" />
-//         <span className="fs-4">SentraPass</span>
-//       </Navbar.Brand>
-//       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//       <Navbar.Collapse id="responsive-navbar-nav">
-//         <Nav className="me-auto">
-//           <Nav.Link as={Link} to="/">Home</Nav.Link>
-//           <Nav.Link as={Link} to="/dash">Dashboard</Nav.Link>
-//           <Nav.Link as={Link} to="/account">My Account</Nav.Link>
-//           <Nav.Link as={Link} to="/aboutcontact">Contact/About</Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-
-// export default Header;
