d4579d7296c3e3c95a0fdbb423987c7920b5a2ab
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-20 18:56:49 +0000

    styles

--- src/Components/DashPageComponents/PassGenComponent/PassGen.jsx
+++ src/Components/DashPageComponents/PassGenComponent/PassGen.jsx
@@ -13,93 +13,86 @@ const PassGen = () => {
   };
 
   return (
-    <div className="container-fluid text-center">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card" id="passGenBody">
-            <div className="card-body">
-              <h1 className="card-title" id="passwordTitle">
-                Password Generation
-              </h1>
-              <label htmlFor="passwordLength" className="form-label" id="passLength">
-                Password Length (8-128 characters):
-              </label>
-              <input
-                type="number"
-                id="passwordLength"
-                min="8"
-                max="128"
-                className="form-control mb-3"
-              />
-              <div className="form-check mb-3">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="upperCase"
-                />
-                <label className="form-check-label" htmlFor="upperCase">
-                  Include Uppercase Characters
-                </label>
-              </div>
-              <div className="form-check mb-3">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="lowerCase"
-                />
-                <label className="form-check-label" htmlFor="lowerCase">
-                  Include Lowercase Characters
-                </label>
-              </div>
-              <div className="form-check mb-3">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="numbers"
-                />
-                <label className="form-check-label" htmlFor="numbers">
-                  Include Numbers
-                </label>
-              </div>
-              <div className="form-check mb-3">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="specialChars"
-                />
-                <label className="form-check-label" htmlFor="specialChars">
-                  Include Special Characters
-                </label>
-              </div>
-              <button
-                onClick={handleGeneratePassword}
-                className="btn btn-primary mb-3"
-              >
-                Generate Password
-              </button>
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Generated Password"
-                  aria-label="Generated Password"
-                  value={password}
-                  readOnly
-                />
-                <button
-                  onClick={() => copyToClipboard(password)}
-                  className="btn btn-outline-secondary"
-                  id="generatedPassword"
-                  type="button"
-                >
-                  <FontAwesomeIcon icon={faCopy} />
-                </button>
-              </div>
-            </div>
+    <div className="col-md-6">
+      <div className="card" id="passGenBody">
+        <div className="card-body">
+          <h1 className="card-title" id="passwordTitle">
+            Password Generation
+          </h1>
+          <label
+            htmlFor="passwordLength"
+            className="form-label"
+            id="passLength"
+          >
+            Password Length (8-128 characters):
+          </label>
+          <input
+            type="number"
+            id="passwordLength"
+            min="8"
+            max="128"
+            className="form-control mb-3"
+          />
+          <div className="form-check mb-3">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="upperCase"
+            />
+            <label className="form-check-label" htmlFor="upperCase">
+              Include Uppercase Characters
+            </label>
           </div>
-        </div>
-        <div className="col-md-6">
-          {/* This empty column ensures the content is on the left-hand side */}
+          <div className="form-check mb-3">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="lowerCase"
+            />
+            <label className="form-check-label" htmlFor="lowerCase">
+              Include Lowercase Characters
+            </label>
+          </div>
+          <div className="form-check mb-3">
+            <input type="checkbox" className="form-check-input" id="numbers" />
+            <label className="form-check-label" htmlFor="numbers">
+              Include Numbers
+            </label>
+          </div>
+          <div className="form-check mb-3">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="specialChars"
+            />
+            <label className="form-check-label" htmlFor="specialChars">
+              Include Special Characters
+            </label>
+          </div>
+          <button
+            onClick={handleGeneratePassword}
+            className="btn btn-primary mb-3"
+          >
+            Generate Password
+          </button>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Generated Password"
+              aria-label="Generated Password"
+              value={password}
+              readOnly
+            />
+            <button
+              onClick={() => copyToClipboard(password)}
+              className="btn btn-outline-secondary"
+              id="generatedPassword"
+              type="button"
+            >
+              <FontAwesomeIcon icon={faCopy} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
