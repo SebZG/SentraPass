b1a20e736b30dae4735f2751d34d9bd90e5691f5
Dr Who Baker <tbaker.bhb@gmail.com>
2024-03-21 18:08:40 +0000

    initialising graph

--- src/Components/DashPageComponents/DataVisualisations/DataBreaches.jsx
+++ src/Components/DashPageComponents/DataVisualisations/DataBreaches.jsx
@@ -1,24 +1,27 @@
 import React, { useState, useCallback, useEffect } from "react";
-import * as d3 from "d3";
+import { csv , scaleBand} from "d3";
 const csvUrl =
-  "https://gist.githubusercontent.com/DrWho1369/1ac5cc3db816e8558e05f6c3db7bb13a/raw/dataBreachesWorldwide.csv";
+  "https://gist.githubusercontent.com/DrWho1369/f759c18dacc0962408c68bd1fd0cf36f/raw/databreaches.csv";
 
-const displayData = (data) => {
-  let message = "";
-  message = message + data.length + " rows\n";
-};
+const width = 960;
+const height = 500;
+// const displayData = (data) => {
+//   let message = "";
+//   message = message + data.length + " rows\n";
+// };
 
 function DataBreaches() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    d3.csv(csvUrl).then((data) => {
-      console.log("Fetched data: ", data);
-      setData(data);
-    });
-  });
-
-  return <div>DataBreaches. {data ? displayData() : "Loading..."}</div>;
+    csv(csvUrl).then(setData)
+    }, []);
+  
+    if (!data) {
+      return <pre>Loading...</pre>;
+    }
+    const yScale = scaleBand()
+  return <svg width={width} height= {height}>{data.map(d => <rect x={0} y={d.y} width={d.width} height={d.height} /></svg>;
 }
 
 export default DataBreaches;
